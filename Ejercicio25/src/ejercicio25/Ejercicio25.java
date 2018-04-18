f2500188ad02f1e97bb58492d54e90729201edbb
arturo <arturo@ArturoMSI>
2018-04-18 09:20:26 +0200

    Ejercicio Finalizado

---
+++ Ejercicio25/src/ejercicio25/Ejercicio25.java
@@ -0,0 +1,16 @@
+package ejercicio25;
+
+/**
+ *
+ * @author Arturo
+ */
+public class Ejercicio25 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Bonoloto b=new Bonoloto();
+    }
+
+}
